6285b3a6542e16787b7dfb187e39bc6d8e273f97
Bade om <71856738+ombade@users.noreply.github.com>
2025-06-26 20:50:52 +0530

    Time: 2 ms (98.94%), Space: 44.8 MB (88.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,48 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer , Integer >num = new HashMap<>();
-        int n = nums.length;
-        for(int i =0 ; i< n ; i++)
-        {
-            // build the hash map
-            num.put(nums[i], i);
-        }
-        // find the complement
-        for(int i =0 ; i< n ; i++)
-        {
-            int comp = target -nums[i];
-            if(num.containsKey(comp) && num.get(comp) != i)
-            {
-                return new int[]{i , num.get(comp)};
-            }
-
-        }
-
-        return new int[]{};
+        // List<integer> ans= new ArrayList();
+    //     for(int i =0 ; i< nums.length ; i++)
+    //     {
+    //         for(int j =i+1; j < nums.length; j++)
+    //         {
+    //             if(nums[i] + nums[j]== target)
+    //             {
+    //                return new int[]{i,j};
+    //             }
+    //         }
+    //     }
+    //     return new int[0];
         
-    }
+    // }
+//     Arrays.sort(nums);
+//     int start = 0;
+//     int end = nums.length -1;
+
+//     while(start < end )
+//     {
+//         int m = nums[start] + nums[end];
+//         if(m == target)
+//         {
+//             return new int[]{nums[start] ,nums[end]};
+//         }
+//         if (m  < target)
+//         {
+//             start ++;
+//         }
+//         else
+//         end --;
+//     }
+// return new int[0];
+HashMap<Integer , Integer> map = new HashMap<>();
+for(int i =0; i< nums.length ; i++)
+{
+    int diff = target - nums[i];
+    if(map.containsKey(diff))
+        {return new int[]{map.get(diff), i};}
+
+    map.put(nums[i] ,i );
 }
+return new int[0] ;
+}
+    }
+    
