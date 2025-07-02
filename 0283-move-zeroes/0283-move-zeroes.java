67a69178fd38cd8136a9ebe1834578fcf2be111b
Bade om <71856738+ombade@users.noreply.github.com>
2025-07-02 17:12:55 +0530

    Time: 2 ms (80.51%), Space: 46.2 MB (44.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,33 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    // public void moveZeroes(int[] nums) {
+    //     int n = nums.length;
+    //     int[] arr = new int[n];
+    //     int cnt = 0;
+    //     for(int i = 0; i< n ; i++)
+    //     {
+    //         if(nums[i] != 0) arr[cnt++] = nums[i];
+    //     }
+    //     for(;cnt < n ; cnt++)
+    //     {
+    //         arr[cnt] =0;
+    //     }
+    //     for( int k = 0 ;  k <n ;k++)
+    //     {
+    //         nums[k]= arr[k];
+    //     }
+    // }
+    public void moveZeroes(int[] nums)
+    {
+        int last = 0;
         int n = nums.length;
-        int[] arr = new int[n];
-        int cnt = 0;
-        for(int i = 0; i< n ; i++)
-        {
-            if(nums[i] != 0) arr[cnt++] = nums[i];
-        }
-        for(;cnt < n ; cnt++)
-        {
-            arr[cnt] =0;
-        }
-        for( int k = 0 ;  k <n ;k++)
-        {
-            nums[k]= arr[k];
-        }
+         for(int i =0 ; i<n ; i++)
+         {
+            if(nums[i] != 0)
+                
+              {  int temp = nums[i];
+                nums[i] = nums[last];
+                nums[last] = temp;
+                last++;}
+         }
     }
 }
