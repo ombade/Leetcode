3dfcfa3c249f13b0f698d2e04f33dd302d3bece4
Bade om <71856738+ombade@users.noreply.github.com>
2025-07-03 12:24:06 +0530

    Time: 2 ms (73.79%), Space: 53.2 MB (38.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,47 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 0 ; i< n ; i++)
-        {
-            int key = nums[i];
-            int cnt = 0;
+    // brute force approch 
+    // public int majorityElement(int[] nums) {
+    //     Arrays.sort(nums);
+    //     int n = nums.length;
+    //     for(int i = 0 ; i< n ; i++)
+    //     {
+    //         int key = nums[i];
+    //         int cnt = 0;
 
-            for(int j = 0 ; j < n ; j++)
-            {
-                if(nums[i]== nums[j])
-                {
-                    cnt++;
-                }
-            }
-            if(cnt > n/2) return nums[i];
-        }
-        return -1;
+    //         for(int j = 0 ; j < n ; j++)
+    //         {
+    //             if(nums[i]== nums[j])
+    //             {
+    //                 cnt++;
+    //             }
+    //         }
+    //         if(cnt > n/2) return nums[i];
+    //     }
+    //     return -1;
         
-    }
+    // }
+     public int majorityElement(int[] nums) {
+        int cnt = 0;
+        int el=0;
+        for(int i = 0; i< nums.length; i++)
+        {
+            if(cnt == 0)
+            {
+                cnt =1;
+                el = nums[i];
+            }
+            else if(nums[i] == el) cnt++;
+            else
+                cnt--;
+        }
+        cnt = 0;
+        for(int j =0 ; j< nums.length ; j++)
+        {
+            if(nums[j] == el) cnt++;
+
+        }
+        if(cnt > nums.length/2) return el;
+        else 
+            return -1;
+     }
 }
