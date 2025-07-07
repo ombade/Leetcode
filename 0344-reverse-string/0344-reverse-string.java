3894e12cf6f038942abe6a4bb63dda56d361ad47
Bade om <71856738+ombade@users.noreply.github.com>
2025-07-07 16:07:52 +0530

    Time: 0 ms (100%), Space: 45.9 MB (83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,18 +1,35 @@
-class Solution {
-      public void solve(char[] s, int start , int end ) {
-      if( start >= end )
-      {
-return ;
-      } 
-      
-      
-      char temp = s[start];
-      s[start] = s[end];
-      s[end] = temp;
-      solve(s , start+1, end-1);
-      }
 
+//Reverse using the Recusresuon 
+// for par
+// class Solution {
+//       public void solve(char[] s, int start , int end ) {
+//       if( start >= end )
+//       {
+// return ;
+//       } 
+      
+      
+//       char temp = s[start];
+//       s[start] = s[end];
+//       s[end] = temp;
+//       solve(s , start+1, end-1);
+//       }
+
+//     public void reverseString(char[] s) {
+//         solve(s , 0 , s.length -1);
+//     }
+// }
+class Solution {
     public void reverseString(char[] s) {
-        solve(s , 0 , s.length -1);
+       int n = s.length;
+       int i = n-1;
+       int j = 0;
+       while(j<i){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j++;
+            i--;
+        }
     }
 }
