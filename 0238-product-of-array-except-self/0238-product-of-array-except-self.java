370897e577b6dd37dd9019e484cb426b2255835a
Bade om <71856738+ombade@users.noreply.github.com>
2025-09-30 10:31:07 +0530

    Time: 2 ms (87.27%), Space: 55.5 MB (50.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,42 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
         int n = nums.length;
-        int pre [] = new int[n];
-        int suff [] = new int[n];
+        int product = 1;
+        int zeroCount = 0;
 
-        pre[0] = 1;
-        suff[n-1] = 1;
-        for(int i = 1 ; i< n ; i++ )
-        {
-            pre[i] = pre[i-1] * nums[i-1];
-        }
-        for(int i = n-2 ; i>= 0 ; i--)
-        {
-            suff[i] = suff[i+1 ] * nums[i+1];
+        // First pass: calculate product of non-zero numbers and count zeros
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else {
+                product *= nums[i];
+            }
         }
 
-        int ans[] = new int[n];
-        for(int i =0 ; i< n ; i++)
-        {
-            ans[i] = pre[i]* suff[i];
+        int[] ans = new int[n];
+
+        // If more than one zero → all results are zero
+        if (zeroCount > 1) {
+            return ans; // already all zeros
         }
+
+        // If exactly one zero → only the position of zero gets product, rest are 0
+        if (zeroCount == 1) {
+            for (int i = 0; i < n; i++) {
+                if (nums[i] == 0) {
+                    ans[i] = product;
+                } else {
+                    ans[i] = 0;
+                }
+            }
+            return ans;
+        }
+
+        // If no zero → safe to divide
+        for (int i = 0; i < n; i++) {
+            ans[i] = product / nums[i];
+        }
+
         return ans;
     }
 }
-// https://leetcode.com/problems/product-of-array-except-self/solutions/1342916/3-minute-read-mimicking-an-interview
