23ebb562fa0e7a8993332a413887b58929e4df90
Bade om <71856738+ombade@users.noreply.github.com>
2025-11-05 10:15:30 +0530

    Time: 2 ms (99.1%), Space: 47.3 MB (5.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,48 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // List<integer> ans= new ArrayList();
-    //     for(int i =0 ; i< nums.length ; i++)
-    //     {
-    //         for(int j =i+1; j < nums.length; j++)
-    //         {
-    //             if(nums[i] + nums[j]== target)
-    //             {
-    //                return new int[]{i,j};
-    //             }
-    //         }
-    //     }
-    //     return new int[0];
-        
-    // }
-//     Arrays.sort(nums);
-//     int start = 0;
-//     int end = nums.length -1;
-
-//     while(start < end )
-//     {
-//         int m = nums[start] + nums[end];
-//         if(m == target)
-//         {
-//             return new int[]{nums[start] ,nums[end]};
-//         }
-//         if (m  < target)
-//         {
-//             start ++;
-//         }
-//         else
-//         end --;
-//     }
-// return new int[0];
-HashMap<Integer , Integer> map = new HashMap<>();
-for(int i =0; i< nums.length ; i++)
-{
-    int diff = target - nums[i];
-    if(map.containsKey(diff))
-        {return new int[]{map.get(diff), i};}
-
-    map.put(nums[i] ,i );
-}
-return new int[0] ;
-}
+        int size = nums.length;
+        Map<Integer , Integer> mp = new HashMap<>();
+        for(int i =0 ; i< size ; i++)
+        {
+            int remaning = target - nums[i];
+            if(mp.containsKey(remaning))
+            {
+                return new int[] {i, mp.get(remaning)};
+            }
+            mp.put(nums[i], i);
+        }
+        return new int [] {};
     }
-    
+}
