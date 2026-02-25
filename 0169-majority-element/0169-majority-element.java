7aa6783c09c370e48c4da738df0bf6940d5dd0e3
Bade om <71856738+ombade@users.noreply.github.com>
2026-02-25 23:34:34 +0530

    Time: 2 ms (70.71%), Space: 56 MB (17.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,67 +1,14 @@
 class Solution {
-   // https://github.com/AlgoMaster-io/leetcode-solutions/blob/main/java/majority-element.md
-    // brute force approch 
-    // public int majorityElement(int[] nums) {
-    //     Arrays.sort(nums);
-    //     int n = nums.length;
-    //     for(int i = 0 ; i< n ; i++)
-    //     {
-    //         int key = nums[i];
-    //         int cnt = 0;
-
-    //         for(int j = 0 ; j < n ; j++)
-    //         {
-    //             if(nums[i]== nums[j])
-    //             {
-    //                 cnt++;
-    //             }
-    //         }
-    //         if(cnt > n/2) return nums[i];
-    //     }
-    //     return -1;
-        
-    // }
-    // my approch
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int cnt = 1;
-        int n = nums.length;
-        if(n ==1) return nums[0];
-        for(int i =1; i< n ; i++)
+        int can = nums[0];
+        int cnt = 0;
+        for(int num :nums)
         {
-            if(nums[i]==nums[i-1])
-            {
-                cnt++;
-                if(cnt > n/2)return nums[i];
-            }
-            else
-                cnt= 1;
-        }
-        return -1;
-    }
-    // optmize approch 
-    //  public int majorityElement(int[] nums) {
-    //     int cnt = 0;
-    //     int el=0;
-    //     for(int i = 0; i< nums.length; i++)
-    //     {
-    //         if(cnt == 0)
-    //         {
-    //             cnt =1;
-    //             el = nums[i];
-    //         }
-    //         else if(nums[i] == el) cnt++;
-    //         else
-    //             cnt--;
-    //     }
-    //     cnt = 0;
-    //     for(int j =0 ; j< nums.length ; j++)
-    //     {
-    //         if(nums[j] == el) cnt++;
+            if(cnt == 0) can = num;
+            cnt += (num == can )?1 :-1; 
 
-    //     }
-    //     if(cnt > nums.length/2) return el;
-    //     else 
-    //         return -1;
-    //  }
+        }
+        return can;
+        
+    }
 }
