7b74bbbb7b5b823b286a9fabd63493c92a87e624
Bade om <71856738+ombade@users.noreply.github.com>
2026-08-01 13:55:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (11.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
-    Arrays.sort(nums);
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        int n = nums.length;
+        for(int i = 0;i < n ; i++){
+            if(nums[i]== 0)zeros++;
+            if(nums[i]== 1) ones++;
+            if(nums[i]== 2) twos++;
+        }
+        for(int i =0 ;i< n ;i++)
+        {
+            if(zeros != 0) 
+            {
+            nums[i] = 0;
+            zeros--;
+            }else if(ones != 0) 
+            {
+            nums[i] = 1;
+            ones--;
+            }
+            else
+            {
+                nums[i]= 2;
+                twos--;
+            }
+
+        }
         
     }
 }
