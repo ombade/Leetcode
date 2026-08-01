3dbd8f89ab151b281289b2900f955aa3cd1281fa
Bade om <71856738+ombade@users.noreply.github.com>
2026-08-01 12:30:29 +0530

    Time: 1 ms (99.96%), Space: 46.5 MB (98.02%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,29 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // int n = nums.length;
-        int res = 0;
-      for(int n : nums )
-      {
-        res = res ^ n;
-      }
-      return res;
+        int xor = 0;
+        for(int i = 0; i< nums.length ;i++)
+        {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+        
     }
-        // for(int i =0 ;i< n ; i++)
-        // {
-        //       boolean sing = true;
-        //     for(int j =0;j<n ;j++)
-        //     {
-        //         if(i != j && nums[i]== nums[j])
-        //         {
-        //             sing = false;
-        //             break;
-        //         }
-        //     }
-        //     if(sing)
-        //     {
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-    // }
 }
