e6a38884e923c08572ecebb74cab581e13ed93c1
Bade om <71856738+ombade@users.noreply.github.com>
2026-08-01 15:50:32 +0530

    Time: 12 ms (37.36%), Space: 52.8 MB (76.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int can = nums[0];
-        int cnt = 0;
-        for(int num :nums)
+        int n = nums.length;
+        HashMap<Integer ,Integer> mp = new HashMap<>();
+        for(int i =0;i< nums.length ; i++)
         {
-            if(cnt == 0) can = num;
-            cnt += (num == can )?1 :-1; 
-
+            mp.put(nums[i], mp.getOrDefault(nums[i],0)+1);
         }
-        return can;
-        
+        for(Map.Entry<Integer , Integer> ent : mp.entrySet())
+        {
+          if  (ent.getValue() > n/2) return ent.getKey();
+        }
+        return -1;
     }
 }
