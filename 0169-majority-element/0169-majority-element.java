c7ea8d1afc77f451068d05bcee730f1201198dc1
Bade om <71856738+ombade@users.noreply.github.com>
2026-08-01 16:08:31 +0530

    Time: 2 ms (70.87%), Space: 55.7 MB (32.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        HashMap<Integer ,Integer> mp = new HashMap<>();
-        for(int i =0;i< nums.length ; i++)
+        // brute force approch 
+        // HashMap<Integer ,Integer> mp = new HashMap<>();
+        // for(int i =0;i< nums.length ; i++)
+        // {
+        //     mp.put(nums[i], mp.getOrDefault(nums[i],0)+1);
+        // }
+        // for(Map.Entry<Integer , Integer> ent : mp.entrySet())
+        // {
+        //   if  (ent.getValue() > n/2) return ent.getKey();
+        // }
+        // return -1;
+        // better approch 
+        int cnt = 0;
+        int ele = 0;
+        for(int i = 0; i< n ; i++)
         {
-            mp.put(nums[i], mp.getOrDefault(nums[i],0)+1);
+            if(cnt == 0)
+            {cnt=1;
+                ele = nums[i];
+            }
+            else if( nums[i]== ele)
+            {
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-        for(Map.Entry<Integer , Integer> ent : mp.entrySet())
+        int cnt1 =0;
+        for(int i = 0; i< n ; i++)
         {
-          if  (ent.getValue() > n/2) return ent.getKey();
+            if(ele == nums[i])cnt1++;
         }
+        if(cnt1 >= n/2)return ele;
         return -1;
     }
 }
