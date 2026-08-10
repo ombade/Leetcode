6826de95d93516eebab460aa4df39b8acf09fd82
Bade om <71856738+ombade@users.noreply.github.com>
2026-08-10 21:50:38 +0530

    Time: 28 ms (7.31%), Space: 47.8 MB (75.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,58 +1,28 @@
 class Solution {
-
-    public long canFinish(int[] piles, int mid) {
-
-        long th = 0;
-
-        for (int i = 0; i < piles.length; i++) {
-
-            th += Math.ceil((double) piles[i] / mid);
+    public static long timerequired(int[] piles , int time )
+    {
+        long ans = 0;
+        for(int i : piles)
+        {
+          ans += Math.ceil((double)i / time);
         }
-
-        return th;
+        return ans;
     }
-
-
-    public int findK(int[] piles, int h, int start, int end, int min) {
-
-        if (start > end) {
-            return min;
-        }
-
-
-        int mid = start + (end - start) / 2;
-
-
-        long hours = canFinish(piles, mid);
-
-
-        if (hours <= h) {
-
-            // Current speed works, try smaller speed
-            min = Math.min(min, mid);
-
-            return findK(piles, h, start, mid - 1, min);
-
-        } 
-        else {
-
-            // Speed is too slow, increase speed
-            return findK(piles, h, mid + 1, end, min);
-        }
-    }
-
-
     public int minEatingSpeed(int[] piles, int h) {
-
-        int end = 0;
-
-
-        // Maximum possible speed
-        for (int pile : piles) {
-            end = Math.max(end, pile);
+        int low = 1;
+        int high =  Arrays.stream(piles).max().getAsInt();
+        while(low <= high)
+        {
+            int mid = (low+high )/2;
+            long ans = timerequired(piles, mid);
+            if( ans <= h)
+            {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
-
-
-        return findK(piles, h, 1, end, Integer.MAX_VALUE);
+        return low;
     }
 }
