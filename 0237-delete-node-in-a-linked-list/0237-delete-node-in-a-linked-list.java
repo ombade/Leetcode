3e04fc6550ff4ef0ba8115ad0d6817b30efa4100
Bade om <ombade364@gmail.com>
2026-08-23 23:21:30 +0530

    Time: 0 ms (100%), Space: 45.2 MB (44.02%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -10,6 +10,8 @@ class Solution {
     public void deleteNode(ListNode node) {
         // while(node.next != null)
         // {
+
+        // slect the next node to delete copy of cuurent not value to be delte add ther the not to be delete value and do according
             node.val = node.next.val;
             node.next = node.next.next;
 // 
